55ff79d73f1960c4b35b27cc7bd5501cfb679ac6
Anderson <sonic1805@Sonic1805>
2025-04-23 19:21:53 -0400

    version finale

--- build.sh
+++ build.sh
@@ -6,12 +6,15 @@ pip install -r requirements.txt
 echo "📁 Création du dossier db s'il n'existe pas..."
 mkdir -p db
 
-echo "📥 Téléchargement de la base de données via gdown..."
-gdown --id 1-yxUYao-6xF_p40qrvKnUxZiO_5Lvxnl -O db/violations_mtl.db
+if [ -f db/violations_mtl.db ]; then
+    echo "⚠️  La base db/violations_mtl.db existe déjà, téléchargement ignoré."
+else
+    echo "📥 Téléchargement de la base de données via gdown..."
+    gdown --id 1-yxUYao-6xF_p40qrvKnUxZiO_5Lvxnl -O db/violations_mtl.db
+fi
 
 echo "📂 Contenu du dossier db :"
 ls -lh db
 
 echo "✅ Build terminé avec succès."
-
 
