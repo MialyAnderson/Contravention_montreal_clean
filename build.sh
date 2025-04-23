db845abfc14013774bce39d33a1581294542f6d6
Anderson <sonic1805@Sonic1805>
2025-04-23 13:44:24 -0400

    version finale

--- build.sh
+++ build.sh
@@ -3,9 +3,14 @@
 echo "📦 Installation des dépendances Python..."
 pip install -r requirements.txt
 
-echo "📥 Téléchargement de la base de données..."
+echo "📁 Création du dossier db s'il n'existe pas..."
 mkdir -p db
-curl -L -o db/violations_mtl.db "https://drive.google.com/uc?export=download&id=1-yxUYao-6xF_p40qrvKnUxZiO_5Lvxnl"
+
+echo "📥 Téléchargement de la base de données via gdown..."
+gdown --id 1-yxUYao-6xF_p40qrvKnUxZiO_5Lvxnl -O db/violations_mtl.db
+
+echo "📂 Contenu du dossier db :"
+ls -lh db
 
 echo "✅ Build terminé avec succès."
 
